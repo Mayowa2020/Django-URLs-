3841e3b011ef35730b9de12aec72db2c14252cf6
Mayowa2020 <73591664+Mayowa2020@users.noreply.github.com>
2022-06-20 06:15:31 +0100

    renamed my project folder to my Zuriboard username

--- aoyetimehin/aoyetimehin/asgi.py
+++ aoyetimehin/aoyetimehin/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for I4G005615YP2 project.
+ASGI config for aoyetimehin project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G005615YP2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aoyetimehin.settings')
 
 application = get_asgi_application()
